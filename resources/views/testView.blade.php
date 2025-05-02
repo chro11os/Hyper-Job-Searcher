914159c013c2bb6b0cd2d47c9b0d254a622e0f93
chro11os <neilbragsg@gmail.com>
2025-05-02 15:25:51 +0000

    Fixed an error with termwind causing to make php artisan serve to not work properly

--- resources/views/testView.blade.php
+++ resources/views/testView.blade.php
@@ -11,22 +11,7 @@
 <body>
     <div class="flex box-border bg-black text-2xl text-amber-50 backdrop-grayscale p-7 m-7 bg-center content-center text-center">
         <h1> Available Jobs </h1>
-
     </div>
-
-    <button onclick="fetchData()">Pull Data (console only)</button>
-    <script>
-        function fetchData() {
-            fetch("{{ route('testView.api') }}")
-                .then(response => response.json())
-                .then(data => {
-                    console.log("Fetched Data:", data);
-                })
-                .catch(error => {
-                    console.error("Error fetching data:", error);
-                });
-        }
-    </script>
 
 </body>
 </html>
